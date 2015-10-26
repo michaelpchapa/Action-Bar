e55f9b882fe72ee8ec1e45ae4997ac4778ad2c61
michaelpchapa <michaelpchapa@gmail.com>
2015-10-26 17:30:03 -0400

    Adding project to github

---
+++ app/src/main/java/com/example/chapa/actionbar/TextFragment.java
@@ -0,0 +1,24 @@
+package com.example.chapa.actionbar;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class TextFragment extends Fragment {
+    TextView text;
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.text_fragment, container, false);
+        text= (TextView) view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
+    }
+
+}
